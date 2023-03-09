cf422e8304fd20112281b5dce09d10ccbceac6ca
Vitor HP Bittencourt <vitorhp2@gmail.com>
2023-03-09 09:03:09 -0300

    Generating QRCode

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import QRCode from 'react-native-qrcode-svg';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <QRCode
+        value="https://www.google.com"
+      />
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
